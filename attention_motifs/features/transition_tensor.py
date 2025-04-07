007d9afcc0d9f50ae3ab7496b69c9d80bf3066b0
Michael Ivanitskiy <mivanits@umich.edu>
2025-04-07 04:17:11 -0600

    wip, starting run

--- attention_motifs/features/transition_tensor.py
+++ attention_motifs/features/transition_tensor.py
@@ -12,8 +12,10 @@ from attention_motifs.util import prefix_dict
 def tt_features(
 	A: Float[np.ndarray, "n_ctx n_ctx"],
 	p_threshold: float = 0.0,
+	device: str = "cuda",
 ) -> dict[str, float]:
-	idxs_tr, tt_tr, _ = transition_tensor_torch(torch.tensor(A, device="cuda"), exact=32, approx_l10=5.0, approx_pts=32)
+	A_torch = torch.tensor(A, device=device)
+	idxs_tr, tt_tr, _ = transition_tensor_torch(A_torch, exact=32, approx_l10=5.0, approx_pts=32)
 	idxs = idxs_tr.cpu().numpy()
 	tt = tt_tr.cpu().numpy()
 	# idxs, tt, _ = transition_tensor(A, exact=32, approx_l10=5.0, approx_pts=32)
