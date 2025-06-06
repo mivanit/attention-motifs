bb81afe51742fc5ec51021a8d1bdf58c399f3098
Michael Ivanitskiy <mivanits@umich.edu>
2025-06-06 16:09:14 -0600

    reorg scripts

--- scripts/download_models.py
+++ scripts/download_models.py
@@ -1,7 +1,14 @@
 from transformer_lens import HookedTransformer
 
-for model_name in "pythia-14m,pythia-1b,tiny-stories-1M,gpt2-small,gpt2-medium,meta-llama/Llama-3.2-1B,gemma-2b".split(
-	","
-):
-	model = HookedTransformer.from_pretrained(model_name)
-	del model
+if __name__ == "__main__":
+	import sys
+	
+	if len(sys.argv) > 2:
+		print("Usage: python download_models.py model_name_1,model_name_2,model_name_n")
+		sys.exit(1)
+
+	models: list[str] = sys.argv[1].split(",")
+
+	for model_name in models:
+		model = HookedTransformer.from_pretrained(model_name)
+		del model
