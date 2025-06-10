fa056dce109fd6690533ac0d12461ee30b6f9df7
Michael Ivanitskiy <mivanits@umich.edu>
2025-06-10 15:25:33 -0600

    ??

---
+++ attention_motifs/frontend/attnpedia/src/headInfo.js
@@ -0,0 +1,28 @@
+class HeadInfo {
+	constructor(id, model, layer, head, paramCount) {
+		this.id = id;
+		this.model = model;
+		this.layer = layer;
+		this.head = head;
+		this.paramCount = paramCount;
+		this.classifications = [];
+		this.citations = [];
+	}
+
+	async getPatterns(promptHash) {
+		// Mock pattern data
+		await new Promise(resolve => setTimeout(resolve, 100));
+		const size = Math.floor(Math.random() * 8) + 8;
+		return {
+			size: size,
+			data: Array(size * size).fill(0).map(() => Math.random())
+		};
+	}
+
+	distanceTo(otherHead, metric = 'L2') {
+		// Mock distance calculation
+		const hash1 = this.id.split('').reduce((a, b) => a + b.charCodeAt(0), 0);
+		const hash2 = otherHead.id.split('').reduce((a, b) => a + b.charCodeAt(0), 0);
+		return Math.abs(hash1 - hash2) / 10000;
+	}
+}
