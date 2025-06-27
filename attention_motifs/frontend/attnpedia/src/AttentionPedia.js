bb495808983d2356ccfd859ecda641d5d61e47e8
Michael Ivanitskiy <mivanits@umich.edu>
2025-06-27 13:58:50 -0700

    wip

--- attention_motifs/frontend/attnpedia/src/AttentionPedia.js
+++ attention_motifs/frontend/attnpedia/src/AttentionPedia.js
@@ -5,53 +5,6 @@ class AttentionPedia {
 		this._initializeMockData();
 	}
 
-	_initializeMockData() {
-		const models = ['gpt2-small', 'gpt2-medium', 'gpt2-large'];
-		const classificationTypes = ['induction', 'duplicate', 'previous', 'name-mover', 'subject', 'copy'];
-		const layers = 12;
-		const headsPerLayer = 12;
-
-		// Generate mock heads
-		models.forEach(model => {
-			const paramCount = model === 'gpt2-small' ? 124 : model === 'gpt2-medium' ? 355 : 774;
-
-			for (let layer = 0; layer < layers; layer++) {
-				for (let head = 0; head < headsPerLayer; head++) {
-					const id = `${model}:L${layer}:H${head}`;
-					const headInfo = new HeadInfo(id, model, layer, head, paramCount);
-
-					// Add random classifications
-					if (Math.random() > 0.3) {
-						const classType = classificationTypes[Math.floor(Math.random() * classificationTypes.length)];
-						headInfo.classifications.push({
-							name: classType,
-							type: classType,
-							source: Math.random() > 0.5 ? 'IOI' : 'SAE-survey'
-						});
-
-						// Add to classification map
-						if (!this.classifications.has(classType)) {
-							this.classifications.set(classType, []);
-						}
-						this.classifications.get(classType).push(headInfo);
-					}
-
-					// Add mock citations
-					if (Math.random() > 0.7) {
-						headInfo.citations.push({
-							title: 'IOI: indirect object identification',
-							authors: 'Wang et al. 2022',
-							url: 'https://arxiv.org/pdf/2211.00593',
-							description: `Model: ${model}`
-						});
-					}
-
-					this.heads.set(id, headInfo);
-				}
-			}
-		});
-	}
-
 	getHead(id) {
 		return this.heads.get(id);
 	}
--- attention_motifs/frontend/attnpedia/src/headInfo.js
+++ attention_motifs/frontend/attnpedia/src/headInfo.js
@@ -1,3 +1,5 @@
+const BASE_PATH = "../../../../docs/demo"
+
 class HeadInfo {
     constructor(id, model, layer, head) {
         this.id = id;
@@ -27,19 +29,7 @@ class HeadInfo {
         }
         return new HeadInfo(id, model, layer, head);
     }
-    async getPatterns(promptHash) {
-        // Mock pattern data
-        await new Promise(resolve => setTimeout(resolve, 100));
-        const size = Math.floor(Math.random() * 8) + 8;
-        return {
-            size: size,
-            data: Array(size * size).fill(0).map(() => Math.random())
-        };
-    }
-    distanceTo(otherHead, metric = 'L2') {
-        // Mock distance calculation
-        const hash1 = this.id.split('').reduce((a, b) => a + b.charCodeAt(0), 0);
-        const hash2 = otherHead.id.split('').reduce((a, b) => a + b.charCodeAt(0), 0);
-        return Math.abs(hash1 - hash2) / 10000;
+    async get_pattern_url(promptHash) {
+        return `${BASE_PATH}/${this.model}/prompts/${promptHash}/L${this.layer}/H${this.head}/raw.png`;
     }
 }
