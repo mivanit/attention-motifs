7544f3e2c503cd688bee6a992a29f70e7cb50e11
Michael Ivanitskiy <mivanits@umich.edu>
2025-06-27 15:22:34 -0700

    wip

--- attention_motifs/frontend/attnpedia/src/AttentionPedia.js
+++ attention_motifs/frontend/attnpedia/src/AttentionPedia.js
@@ -1,16 +1,45 @@
+const ATTNPEDIA_URL = "ap.json";
+
+async function load_attnpedia(path = ATTNPEDIA_URL) {
+	const r = await fetch(path);
+	if (r.ok) {
+		const loaded = await r.json();
+		return this._load(loaded);
+	} else {
+		throw new Error(`Failed to load AttentionPedia data from ${path}`);
+	}
+}
+
 class AttentionPedia {
 	constructor() {
-		this.heads = new Map();
-		this.classifications = new Map();
-		this._initializeMockData();
+		this._data = load_attnpedia();
 	}
 
-	getHead(id) {
-		return this.heads.get(id);
+	get_head_types(head_id) {
+		if (head_id in this._data.head_to_types) {
+			return this._data.head_to_types[head_id];
+		}
+		else {
+			return [];
+		}
 	}
 
-	getHeadsByClassification(classification) {
-		return this.classifications.get(classification) || [];
+	get_type_heads(type) {
+		if (type in this._data.type_to_heads) {
+			return this._data.type_to_heads[type];
+		}
+		else {
+			return [];
+		}
+	}
+
+	get_type_meta(type) {
+		if (type in this._data.type_metadata) {
+			return this._data.type_metadata[type];
+		}
+		else {
+			return {};
+		}
 	}
 
 	getNearestHeads(headId, n = 5) {
@@ -33,3 +62,5 @@ class AttentionPedia {
 			.map(item => ({ ...item.head, distance: item.distance }));
 	}
 }
+
+let ATTENTION_PEDIA = new AttentionPedia();
